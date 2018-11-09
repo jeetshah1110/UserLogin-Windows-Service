d896b1d45e9655a97e1ec796eae02df976864aa6
jeetshah1110 <36303896+jeetshah1110@users.noreply.github.com>
2018-11-09 23:30:50 +0530

    Commit 1

---
+++ Service1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.DirectoryServices.AccountManagement;
+using System.Security.Principal;
+namespace UserLoginService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            
+            
+            
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\Jeet\Desktop\UserInfo.txt",append:true))
+            {
+
+
+                WindowsPrincipal wp = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+                String username = wp.Identity.Name;
+                sw.WriteLine("User Name: "+username+"        Login Time: "+ GetLastLoginToMachine(Environment.MachineName, username));
+                
+
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+        public static string GetLastLoginToMachine(string machineName, string userName)
+        {
+            string op = null;
+            PrincipalContext c = new PrincipalContext(ContextType.Machine, machineName);
+            UserPrincipal uc = UserPrincipal.FindByIdentity(c, userName);
+            op = Convert.ToString(uc.LastLogon);
+            return op;
+        }
+    }
+}
